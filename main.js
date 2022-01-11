359f1f09b272c5a5a99d1e38a1b4e47e0c17206b
TP-Interprice <temuriy1998@gmail.com>
2022-01-11 23:53:14 +0500

    Cristiano Ronaldo

---
+++ main.js
@@ -0,0 +1,11 @@
+// alert("Assalom aleykum. Hush kelibsiz loyihamga!")
+// confirm("Yoshingiz 18 dan oshganmi ?")
+// prompt("Unda yoshingizni kiriting:", " Temurbek ")
+
+// var x = 24;
+
+// alert(x);
+
+// var x = 24;
+// console.log(x)
+
